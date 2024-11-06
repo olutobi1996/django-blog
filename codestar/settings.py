81f8dc078403e5a55855029268b8cf0ee0b6e7bd
olutobi Adebayo <olutobi1996@icloud.com>
2024-11-06 12:45:16 +0000

    views models added

--- codestar/settings.py
+++ codestar/settings.py
@@ -77,14 +77,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'codestar.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#Database
+#https:docs.djangoproject.com/en/4.2/ref/settings/databases
 
-# DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
+#DATABASES = {
+#  'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
 #}
 
 DATABASES = {
